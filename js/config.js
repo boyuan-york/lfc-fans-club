1be12a14557905cab950ac3f7ebb963c5be1d431
boyuan-york <cdx522@york.ac.uk>
2026-05-17 15:29:29 +0100

    feat: genxin

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
  * Copy this file to config.js and paste your Supabase project credentials.
  * See ../references/SUPABASE_SETUP.md (from FootBallll root).
  */
-export const SUPABASE_URL = "https://vgryagqprgbgnjbgnubm.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_PY9UIIa3Sxu6mOhMab_L9w_UVAwx9u8";
+export const SUPABASE_URL = "xxx";
+export const SUPABASE_ANON_KEY = "xxx";
